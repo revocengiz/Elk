927796e46acc7ba2bc406c638cdfc3c603aab98b
cengiz.bulut <cengiz.bulut@eplatform.com.tr>
2017-08-09 11:27:04 +0300

    Added Sample

---
+++ Sample/ElasticSink/Program.cs
@@ -0,0 +1,29 @@
+﻿using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System;
+
+namespace ElasticSink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loggerConfig = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+            {
+                AutoRegisterTemplate = true,
+            });
+
+
+            var logger = loggerConfig.CreateLogger();
+
+            for (int i = 0; i < 5; i++)
+            {
+                logger.Information("The {user} has access to {resource}", "cengiz", $"resource-{i}");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Sample/HttpSink/Program.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+using System;
+
+namespace HttpSink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger log = new LoggerConfiguration()
+                    .Enrich.WithProperty("CorrelationId", Guid.NewGuid().ToString())
+                    .Enrich.FromLogContext()
+                    .MinimumLevel.Verbose()
+                    .WriteTo.DurableHttp("http://localhost:31311")
+                    .CreateLogger()
+                    .ForContext<HttpSink.Program>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                log.Information("The {user} has access to {resource}", "cengiz", $"resource-{i}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
